bdf754ffc71647e654a92b32e2f9994c39a461aa
Silvia Calcaterra <silvia.calcaterra@hotmail.it>
2021-06-26 13:39:40 +0300

    Exercise 2.5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,39 +1,5 @@
 import React from 'react'
-
-const Course = ({course}) => {
-  return (
-    <div>
-      <Header course={course}/>
-      <Content course={course}/>
-      <Total course={course}/>
-    </div>
-  )
-}
-
-const Header = ({ course }) => {
-  return (
-    <h2>{course.name}</h2>
-  )
-}
-
-const Total = ({ course }) => {
-  const sum = course.parts.reduce((sum, part) => sum + part.exercises, 0)
-  return(
-    <p>total of {sum} exercises</p>
-  ) 
-}
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part.name} {props.part.exercises}
-    </p>    
-  )
-}
-
-const Content = ({ course }) => {
-  return course.parts.map(p => <Part key={p.id} part={p} />)
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
