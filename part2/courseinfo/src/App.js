56fbb3d85a33b7062ba4682dd5c798791daeed6e
Silvia Calcaterra <silvia.calcaterra@hotmail.it>
2021-06-26 13:31:18 +0300

    Exercise 2.3

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -17,7 +17,7 @@ const Header = ({ course }) => {
 }
 
 const Total = ({ course }) => {
-  const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
+  const sum = course.parts.reduce((sum, part) => sum + part.exercises, 0)
   return(
     <p>Number of exercises {sum}</p>
   ) 
@@ -54,6 +54,11 @@ const App = () => {
         name: 'State of a component',
         exercises: 14,
         id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
       }
     ]
   }
