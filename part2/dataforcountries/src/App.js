14c24eeca6c5fcea2b623f4d512927f3827db24a
Silvia Calcaterra <silvia.calcaterra@hotmail.it>
2021-06-29 22:43:43 +0300

    Exercise 2.14

--- part2/dataforcountries/src/App.js
+++ part2/dataforcountries/src/App.js
@@ -8,17 +8,44 @@ const SearchFilter = ({ filter, onFilterChange}) => (
   </div>
 )
 
+const WeatherForecast = ({ forecast }) => {
+  if (forecast === '') return (<></>)
+
+    return (
+      <div>
+         <p><b>Temperature:</b> {forecast.main.temp} °C</p>
+         <img src={`http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`} alt={forecast.weather[0].description}></img>
+         <p><b>Wind:</b> {forecast.wind.speed} m/s, direction {forecast.wind.direction} degrees</p>
+      </div>
+     )
+}
+
 const Country = ({ country }) => {
+  const [ weather, setWeather ] = useState('')
+  
+  const hook = () => {
+    axios
+    .get(`https://api.openweathermap.org/data/2.5/weather?q=${country.capital}&units=metric&appid=${process.env.REACT_APP_API_KEY}`)
+    .then(response => {
+      console.log('forecast promise fulfilled')
+      setWeather(response.data)
+    })
+  }
+
+  useEffect(hook, [country.capital])
+
   return (
     <div>
-      <h1>{country.name}</h1>
+      <h2>{country.name}</h2>
       <p>Capital {country.capital}<br/>
       Population {country.population}</p>
       <h3>Languages</h3>
       <ul>
         {country.languages.map(language => <li key={language.name}>{language.name}</li>)}
       </ul>
-      <img src={country.flag} width="125" height="125"/>
+      <img src={country.flag} width="200" height="125" alt={`${country.name} flag`}/>
+      <h3>Weather in {country.capital}</h3>
+      <WeatherForecast forecast={weather}/>
     </div>
   )
 }
@@ -60,11 +87,10 @@ const App = () => {
   }
 
   const hook = () => {
-    console.log('effect')
     axios
     .get('https://restcountries.eu/rest/v2/all')
     .then(response => {
-      console.log('promise fulfilled')
+      console.log('countries data promise fulfilled')
       setCountries(response.data)
     })
   }
@@ -81,7 +107,6 @@ const App = () => {
       <SearchFilter filter={filter} onFilterChange={handleFilterChange} />
       <Countries countries={shownCountries} onCountryClick={(country) => setInspectedCountry(country)}/>
     </div>
-    
   )
 }
 
