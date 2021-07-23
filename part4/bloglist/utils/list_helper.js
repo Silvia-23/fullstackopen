6e74f6a970612b4a9b7e6d3e575ff0193447c169
Silvia Calcaterra <silvia.calcaterra@hotmail.it>
2021-07-23 15:22:37 +0200

    Exercise 4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,19 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const reducer = (sum, likes) => {
+    return sum + likes
+  }
+
+  return blogs.length === 0
+    ? 0
+    : blogs
+      .map(blog => blog.likes)
+      .reduce(reducer, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
