5e2ee5b355abff45a88735c7264f1a14a3331ba0
Silvia Calcaterra <silvia.calcaterra@hotmail.it>
2021-07-23 19:25:21 +0200

    Exercise 4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -14,7 +14,14 @@ const totalLikes = (blogs) => {
       .reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const sortedBlogs = blogs
+  .sort((blog1, blog2) => blog2.likes - blog1.likes)
+  return sortedBlogs[0]
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
