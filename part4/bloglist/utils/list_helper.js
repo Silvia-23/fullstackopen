a8067fb0901d2ea2b0b0baa6c4f11e253fda2bd1
Silvia Calcaterra <silvia.calcaterra@hotmail.it>
2021-07-27 10:38:52 +0200

    Exercise 4.8

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const dummy = (blogs) => {
   return 1
 }
@@ -15,13 +17,33 @@ const totalLikes = (blogs) => {
 }
 
 const favoriteBlog = (blogs) => {
-  const sortedBlogs = blogs
+  const sortedBlogs = [...blogs]
   .sort((blog1, blog2) => blog2.likes - blog1.likes)
+
   return sortedBlogs[0]
+}
+
+//TODO EXERCISES 4.6 AND 4.7 ARE MISSING|
+const mostBlogs = (blogs) => {
+  let authorsAndPosts = [...blogs]
+  authorsAndPosts = _.groupBy(authorsAndPosts, blog => blog.author)
+  _.map(authorsAndPosts, blog => ({ author: blog.key, posts: blog.values.length }))
+  //console.log(_.maxBy(authorsAndPosts, blogList => blogList.values.length))
+  return authorsAndPosts.values[0]
+}
+
+const mostLikes = (blogs) => {
+  console.log(_.chain([...blogs])
+    .groupBy(blog => blog.author)
+    .reduce((sum, blogList) => sum + blogList.likes).value())
+  return _.chain([...blogs])
+    .maxBy(blog => blog.likes)
 }
 
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
 }
