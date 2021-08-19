5b7775285649a84fade1ff8c29b98e85f0b0acea
Silvia Calcaterra <silvia.calcaterra@hotmail.it>
2021-08-19 19:03:13 +0200

    Exercise 5.3

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -10,6 +10,9 @@ const App = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
+  const [blogTitle, setBlogTitle] = useState('')
+  const [blogAuthor, setBlogAuthor] = useState('')
+  const [blogUrl, setBlogUrl] = useState('')
 
   const localStorageUserKey = 'loggedBloglistAppUser'
 
@@ -24,6 +27,7 @@ const App = () => {
     if (loggedUserJSON) {
       const user = JSON.parse(loggedUserJSON)
       setUser(user)
+      blogService.setToken(user.token)
     }
   }, [])
 
@@ -39,6 +43,8 @@ const App = () => {
       window.localStorage.setItem(
         localStorageUserKey, JSON.stringify(user)
       )
+
+      blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
@@ -55,6 +61,28 @@ const App = () => {
 
     window.localStorage.removeItem(localStorageUserKey)
     setUser(null)
+  }
+
+  const handleNewBlog = async (event) => {
+    event.preventDefault()
+    console.log('inserting new blog with: ', blogTitle, blogAuthor, blogUrl)
+
+    try {
+      const newBlog = {
+        title: blogTitle, author: blogAuthor, url: blogUrl
+      }
+      const blog = await blogService.createBlog(newBlog)
+
+      setBlogTitle('')
+      setBlogAuthor('')
+      setBlogUrl('')
+      setBlogs(blogs.concat(blog))
+    } catch (exception) {
+      setErrorMessage('Blog insertion failed')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
   }
 
   if (user === null) {
@@ -94,6 +122,37 @@ const App = () => {
         {user.name} logged in
         <button onClick={handleLogout}>logout</button>
       </p>
+      <h2>Create New</h2>
+        <form onSubmit={handleNewBlog}>
+          <div>
+            title:
+            <input
+                type="text"
+                value={blogTitle}
+                name="BlogTitle"
+                onChange={({ target }) => setBlogTitle(target.value)}
+              />
+          </div>
+          <div>
+            author:
+            <input
+                type="text"
+                value={blogAuthor}
+                name="BlogAuthor"
+                onChange={({ target }) => setBlogAuthor(target.value)}
+              />
+          </div>
+          <div>
+            url:
+            <input
+                type="text"
+                value={blogUrl}
+                name="BlogUrl"
+                onChange={({ target }) => setBlogUrl(target.value)}
+              />
+          </div>
+          <button type="submit">create</button>
+        </form>
       {blogs.map(blog =>
           <Blog key={blog.id} blog={blog} />)}
     </div>
