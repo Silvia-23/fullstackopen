4ca42e701da3ac4f9b052e7997d6debef1f30d64
Silvia Calcaterra <silvia.calcaterra@hotmail.it>
2021-08-27 21:21:00 +0200

    Exercise 5.11

--- part5/bloglist-frontend/src/components/BlogForm.js
+++ part5/bloglist-frontend/src/components/BlogForm.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import PropTypes from 'prop-types'
 
 const BlogForm = ({ createBlog }) => {
   const [blogTitle, setBlogTitle] = useState('')
@@ -51,6 +52,10 @@ const BlogForm = ({ createBlog }) => {
         </form>
   </div>
   )
+}
+
+BlogForm.propTypes = {
+  createBLog: PropTypes.func.isRequired
 }
 
 export default BlogForm
