f33778087980e3586bfee8feed11b0c370037c3c
Silvia Calcaterra <silvia.calcaterra@hotmail.it>
2021-08-28 14:24:01 +0200

    Exercise 5.14

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,26 +1,42 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
-test('renders author and title, but not additional info by default', () => {
-  const blog = {
-    name: 'Blog Name',
-    author: 'Blog Author',
-    likes: 2,
-    url: 'www.blogname.it',
-    user: {
-      name: 'username'
+describe('<Blog />', () => {
+  let component
+
+  beforeEach(() => {
+    const blog = {
+      name: 'Blog Name',
+      author: 'Blog Author',
+      likes: 2,
+      url: 'www.blogname.it',
+      user: {
+        name: 'username'
+      }
     }
-  }
 
-  const component = render(
-    <Blog blog={blog} />
-  )
+    component = render(
+      <Blog blog={blog} />
+    )
+  })
 
-  const div = component.container.querySelector('.blog')
-  expect(div).not.toHaveStyle('display: none')
+  test('renders author and title, but not additional info by default', () => {
+    const div = component.container.querySelector('.blog')
+    expect(div).not.toHaveStyle('display: none')
 
-  const togglableDiv = component.container.querySelector('.togglableInfo')
-  expect(togglableDiv).toHaveStyle('display: none')
+    const togglableDiv = component.container.querySelector('.togglableInfo')
+    expect(togglableDiv).toHaveStyle('display: none')
+  })
+
+  test('after clicking the button, additional info is displayed', () => {
+    const button = component.getByText('show')
+    fireEvent.click(button)
+
+    const div = component.container.querySelector('.togglableInfo')
+    expect(div).not.toHaveStyle('display: none')
+    expect(div).toHaveTextContent('www.blogname.it2 likeslikeusernameremove')
+  })
+
 })
